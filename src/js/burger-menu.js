96827987238b7951f462d3bc8200a36a9309b819
Bogdan Teteriuk <exxinblood@gmail.com>
2025-01-27 16:23:47 +0200

    fix modal menu

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,6 +1,18 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
+let btnMenuMob = document.querySelector('[btn-menu-mob]');
+let menuLinks = document.querySelectorAll('.item-h__link');
+
 menuBtn.addEventListener('click', function () {
   menuBtn.classList.toggle('active');
   menu.classList.toggle('active');
+  btnMenuMob.classList.toggle('active');
+});
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', function () {
+    menuBtn.classList.remove('active');
+    menu.classList.remove('active');
+    btnMenuMob.classList.remove('active');
+  });
 });
